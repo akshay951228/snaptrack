c48e8255e1f00504283cdb7912505b9ab6e0d052
akshay kumar <akshay951228@gmail.com>
2017-09-09 22:16:08 +0530

    added google sigin

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,49 @@ import React,{Component} from 'react';
 import {BrowserRouter,Route,Link} from 'react-router-dom';
 import {Menu} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import * as firebase from 'firebase';
 
 
 export default class Routes extends Component{
+
+    constructor(props){
+            super(props);
+
+    }
+    
+
+    handleGoogleLogin(){
+        let provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+    }
+
+
+    componentDidMount(){
+        firebase.auth().getRedirectResult().then(this.authRedirectSuccess).catch(this.authRedirectFail);
+        
+        firebase.auth().onAuthStateChanged(this.handleAuthChange);
+        
+    }
+    authRedirectSuccess(result){
+        console.log(result);
+    }
+    authRedirectFail(result){
+        console.log(result);
+    }
+    handleAuthChange(user){
+        console.log(user);
+    }
+
     render(){
         return(
             <BrowserRouter>
                 <div>
                 <Menu>
         <Menu.Item name='SnapTrack' header>SNAPTRACK</Menu.Item>
+        <Menu.Menu position='right'>
+            
+          <Menu.Item name='signup'  onClick={this.handleGoogleLogin}>Google</Menu.Item>
+          </Menu.Menu>
             </Menu>
                 </div>
             </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBf-eLykJYGmUtAG8_YGx3bzX3Djmw5_ME",
+  authDomain: "snaptrack-f1851.firebaseapp.com",
+  databaseURL: "https://snaptrack-f1851.firebaseio.com",
+  projectId: "snaptrack-f1851",
+  storageBucket: "",
+  messagingSenderId: "40575916475"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <Routes />,
